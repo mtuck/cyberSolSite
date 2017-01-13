4974aab7c18a03b83ac20ec711a78e22327a20e4
mtuck <Mtuck@una.edu>
2017-01-13 14:39:36 -0600

    page scroll

--- js/Scrolling-nav.js
+++ js/Scrolling-nav.js
@@ -13,6 +13,41 @@ $(window).scroll(function () {
 });
 */
 
+$(window).on("load",function() {
+    function fade() {
+        var animation_height = $(window).innerHeight() * 0.25;
+        var ratio = Math.round( (1 / animation_height) * 10000 ) / 10000;
+
+        $('.fade').each(function() {
+
+            var objectTop = $(this).offset().top;
+            var windowBottom = $(window).scrollTop() + $(window).innerHeight();
+
+            if ( objectTop + 500 < windowBottom ) {
+                if ( objectTop < windowBottom - animation_height  ) {
+
+                    $(this).css( {
+                        transition: 'opacity 0.5s linear',
+                        opacity: 1
+                    } );
+
+                } else {
+
+                    $(this).css( {
+                        transition: 'opacity 0.5s linear',
+                        opacity: (windowBottom - objectTop) * ratio
+                    } );
+                }
+            } else {
+
+                $(this).css( 'opacity', 0 );
+            }
+        });
+    }
+    $('.fade').css( 'opacity', 0 );
+    fade();
+    $(window).scroll(function() {fade();});
+});
 //jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function () {
     $('a.page-scroll').bind('click', function (event) {
