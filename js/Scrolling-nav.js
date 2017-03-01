d268d3274156d784f93607dc390fc65ee5aecd7e
Martin Tuck <Mtuck@una.edu>
2017-02-28 18:30:34 -0600

    new site

--- js/Scrolling-nav.js
+++ js/Scrolling-nav.js
@@ -1,6 +1,22 @@
 
 
 $(window).on("load",function() {
+
+    $(function() {
+        $('a[href*="#"]:not([href="#"])').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+
     function fade() {
         var animation_height = $(window).innerHeight() * 0.25;
         var ratio = Math.round( (1 / animation_height) * 10000 ) / 10000;
